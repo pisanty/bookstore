2cf06163fc2c3e9005bf888e640cc39f0e1e3878
pisanty <pisanty@gmail.com>
2021-10-14 15:30:03 +0300

    second commit

---
+++ src/main/java/com/example/demobookstore2021/model/BookRepository.java
@@ -0,0 +1,13 @@
+package com.example.demobookstore2021.model;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+	
+	List<Book> findByAuthor(String author);
+
+}
